4a02d1d66f78d17dceefd15c342694da73ecbacc
Enrique Shunnar <xtealer00@gmail.com>
2020-02-23 17:31:27 -0500

    added nes emulator deployment files

---
+++ nes/precache-manifest.62f3995a4b2e716a6066073f493bb700.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87fd48a4661c2e1076ebd435d6c8a9a0",
+    "url": "/nes/index.html"
+  },
+  {
+    "revision": "878d1c4be8d7e9fc4a95",
+    "url": "/nes/static/css/main.9cfc2e31.chunk.css"
+  },
+  {
+    "revision": "622b92431e90f3c14d92",
+    "url": "/nes/static/js/2.e4fbc359.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/nes/static/js/2.e4fbc359.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "878d1c4be8d7e9fc4a95",
+    "url": "/nes/static/js/main.5b222c20.chunk.js"
+  },
+  {
+    "revision": "a386b951c9996161ffd2",
+    "url": "/nes/static/js/runtime-main.988b51e6.js"
+  }
+]);
