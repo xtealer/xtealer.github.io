c7cd74dec1871f82e27a0ab8b26c9321b3e2ad2d
Enrique Shunnar <xtealer00@gmail.com>
2020-02-24 02:56:37 -0500

    add eurus 2019 hackathon project

---
+++ eurus2019/precache-manifest.5cd21ee547c3df70175303e7a7512463.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6580a8cdf6ec8e20c1a24159d22eda70",
+    "url": "/eurus2019/index.html"
+  },
+  {
+    "revision": "32521a7d650c49826e75",
+    "url": "/eurus2019/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "48f1f36eb8e79ca11cb6",
+    "url": "/eurus2019/static/css/main.28d4e0a2.chunk.css"
+  },
+  {
+    "revision": "32521a7d650c49826e75",
+    "url": "/eurus2019/static/js/2.8f69bf7a.chunk.js"
+  },
+  {
+    "revision": "f9e94c0ffdf58e6c99db5403b6d47052",
+    "url": "/eurus2019/static/js/2.8f69bf7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48f1f36eb8e79ca11cb6",
+    "url": "/eurus2019/static/js/main.8aba1872.chunk.js"
+  },
+  {
+    "revision": "3c0b6c1f1a512213895a",
+    "url": "/eurus2019/static/js/runtime-main.08f73072.js"
+  },
+  {
+    "revision": "8a7d4e107cdc7103752b2915aa698466",
+    "url": "/eurus2019/static/media/58.8a7d4e10.jpg"
+  },
+  {
+    "revision": "a9b370b0ab661416e5248433a3ddcba2",
+    "url": "/eurus2019/static/media/bad-thumb.a9b370b0.png"
+  },
+  {
+    "revision": "07ff6d4746fe678b4929ea1f896a754e",
+    "url": "/eurus2019/static/media/batteries.07ff6d47.gif"
+  },
+  {
+    "revision": "a2541a0bbe52ed70960566439c048f30",
+    "url": "/eurus2019/static/media/good-thumb.a2541a0b.jpg"
+  },
+  {
+    "revision": "3dca4257251658908822b7d0d41c1db2",
+    "url": "/eurus2019/static/media/mercecharge.3dca4257.jpg"
+  },
+  {
+    "revision": "d10e8ee70313776d33b7c2c1f0835478",
+    "url": "/eurus2019/static/media/recharge.d10e8ee7.jpg"
+  }
+]);
